93751311a41ae5b271559064ad128131a3f53753
affan14 <affan.ahmad@yahoo.de>
2025-07-08 23:53:26 +0200

    Updated project diary

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+
+
 function Header() {
   return (
     <header className="bg-white shadow-md py-4 px-6 flex justify-between items-center">
@@ -14,8 +16,8 @@ function Header() {
 
       {/* Rechts: Login/Registrieren/Profile */}
       <div className="flex items-center space-x-4">
-        <a href="/login" className="text-gray-700 hover:text-green-600 transition-colors">Login</a>
-        <a href="/register" className="text-gray-700 hover:text-green-600 transition-colors border border-green-600 rounded px-3 py-1 ml-2 hover:bg-green-50">Registrieren</a>
+        <a href="/Login" className="text-gray-700 hover:text-green-600 transition-colors">Login</a>
+        <a href="/Register" className="text-gray-700 hover:text-green-600 transition-colors border border-green-600 rounded px-3 py-1 ml-2 hover:bg-green-50">Registrieren</a>
         <button className="relative">
           <img
             src="/user-avatar.png"
---
+++ project-files/Projekt-Tagebuch_08072025.md
@@ -0,0 +1,22 @@
+
+Tag 7 - 08.07.2025:
+
+Heutige Hauptaufgabe(n):
+ 
+- An folgenden Sprints heute gearbeitet:  MYM-8, MYM-9
+
+Fortschritt & Ergebnisse: 
+
+- Leider keine Fortschritte
+
+Herausforderungen & Blockaden:
+
+- Erstellung der Registrierung Seite 
+
+
+Was ich heute gelernt habe: 
+
+
+Plan für morgen: (Was ist der nächste logische Schritt?)
+
+- Formulare/Pages für Login, Registrierung erstellen, verfeineren.
